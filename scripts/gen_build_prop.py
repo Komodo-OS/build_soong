fbb353b8e3f215522ccbee8b5d99e8534fecb50a
Mikazuuu07 <mikazuuu07@komodo-os.my.id>
2025-02-21 21:38:18 +0700

    soong: Init KomodoOS

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -118,8 +118,8 @@ def parse_args():
   if args.build_thumbprint_file:
     config["BuildThumbprint"] = args.build_thumbprint_file.read().strip()
 
-  config["LineageDesc"] = config["BuildDesc"]
-  config["LineageDevice"] = config["DeviceName"]
+  config["KomodoDesc"] = config["BuildDesc"]
+  config["KomodoDevice"] = config["DeviceName"]
 
   if config["BuildNumber"].startswith("eng."):
     config["BuildNumber"] = config["DateUtc"]
@@ -221,7 +221,7 @@ def generate_build_info(args):
       print(f"ro.build.display.id?={config['BuildId']} {config['BuildKeys']}")
   else:
     # Non-user builds should show detailed build information (See build desc above)
-    print(f"ro.build.display.id?={config['LineageDesc']}")
+    print(f"ro.build.display.id?={config['KomodoDesc']}")
   print(f"ro.build.version.incremental={config['BuildNumber']}")
   print(f"ro.build.version.sdk={config['Platform_sdk_version']}")
   print(f"ro.build.version.preview_sdk={config['Platform_preview_sdk_version']}")
@@ -248,7 +248,7 @@ def generate_build_info(args):
   # flavor (via a dedicated lunch config for example).
   print(f"ro.build.flavor={config['BuildFlavor']}")
 
-  print(f"ro.lineage.device={config['LineageDevice']}")
+  print(f"org.komodo.device={config['KomodoDevice']}")
 
   # These values are deprecated, use "ro.product.cpu.abilist"
   # instead (see below).
